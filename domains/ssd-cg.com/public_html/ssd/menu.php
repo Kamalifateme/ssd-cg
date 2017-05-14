5f7752f593e926a838e5aaf13bb776d4740b5a22
kalantari2017 <kavirgirl2012@gmail.com>
2017-05-14 13:56:50 +0430

    اضافه کردن اسنیپت

--- domains/ssd-cg.com/public_html/ssd/menu.php
+++ domains/ssd-cg.com/public_html/ssd/menu.php
@@ -1,16 +1,21 @@
 
 
 <section id="Education">
-		<h1  title="ثبت نام از طریق سایت گروه حرفه ای مشاوران کارآفرینی و کسب و کار ssd">آموزش</h1>
+		<h1 itemscope itemtype="http://schema.org/headline itemprop="Education" title="ثبت نام از طریق سایت گروه حرفه ای مشاوران کارآفرینی و کسب و کار ssd | Education | register">آموزش</h1>
 
 		<ul class="cols-4 ca-menu" style="margin-top:60px;margin-bottom:60px;margin-right: -20px;margin-left:-15px;">
-		<div itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="learning" class="mmenus">
 			
-                    <li itemscope itemtype="http://schema.org/WebSite" itemprop="url" id="learning"  style="">
+                    <li itemscope itemtype="http://schema.org/url" itemprop="learning" id="learning"  style="">
                         <a href="<?php echo $path; ?>learning-courses" >
                             <div class="ca-content">
+<<<<<<< HEAD
                                 <h2 title="دوره های یادگیری مجازی و حضوری کارافرینی و کسب و کار ssd" class="ca-main" style="font-size:28px;">دوره های یادگیری</h2>
                                 <h3 title="نام دوره های یادگیری مجازی و حضوری کارافرینی و کسب و کار ssd" class="ca-sub">
+=======
+                                <h2 title="نام دوره یادگیری مجازی و حضوری کارافرینی و کسب و کار ssd" class="ca-main" style="font-size:26px;">دوره های یادگیری</h2>
+                                <h3 title="شرح دوره یادگیری مجازی و حضوری کارافرینی و کسب و کار ssd" class="ca-sub">
+
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -25,8 +30,8 @@
                     </li>
 </div>
 
-		<div itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
-                    <li itemscope itemtype="http://schema.org/WebSite" itemprop="url" id="reg">
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="registration" class="mmenus">
+                    <li itemscope itemtype="http://schema.org/url" itemprop="registration" id="reg">
                         <a href="<?php echo $path; ?>reg" style="">
                             <div class="ca-content">
                                 <h2 title="دوره های آموزشی مجازی و حضوری کارافرینی و کسب و کار ssd" class="ca-main">آموزش آنلاین</h2>
@@ -45,11 +50,11 @@
                     </li>
 			</div>
 
-		<div itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
-                    <li itemscope itemtype="http://schema.org/WebSite" itemprop="url" id="archive">
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="archive-images" class="mmenus">
+                    <li itemscope itemtype="http://schema.org/url" itemprop="archive-images" id="archive">
                         <a href="<?php echo $path; ?>archive-images" style="">
-                            <div itemscope itemtype="http://schema.org/image" itemprop="photo" class="ca-content">
-                                <h2 title="تصاویر دوره های مجازی و حضوری کارآفرینی و کسب و کار ssd" class="ca-main">آرشیو تصاویر</h2>
+                            <div itemscope itemtype="http://schema.org/photo" itemprop="learning-course" class="ca-content">
+                                <h2 title="تصاویر دوره های مجازی و حضوری کارآفرینی و کسب و کار ssd | online learning course" class="ca-main">آرشیو تصاویر</h2>
                                 <h3 title="نام تصاویر دوره های مجازی و حضوری کارآفرینی و کسب و کار ssd" class="ca-sub">
 			<?php
 				mysql_query("SET CHARACTER SET utf8");   
@@ -64,11 +69,11 @@
                     </li>								
 			</div>
 			
-		<div itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="ssd-education" class="mmenus">
                     <li id="education" style="">
                         <a href="<?php echo $path; ?>ssd-education" >
                             <div class="ca-content">
-                                <h2 title="چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd" class="ca-main">چرا SSD</h2>
+                                <h2 title="چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd | why ssd" class="ca-main">چرا SSD</h2>
                                 <h3 title="شرح چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd" class="ca-sub">
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
@@ -93,15 +98,20 @@
 	
 	<section id="Consultation">
 
-	<h1 title="گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار">مشاوره</h1>
-		<ul itemscope itemtype="http://schema.org/WebSite" itemprop="hasPart" class="cols-4 ca-menu" style="margin-top:60px;margin-bottom:60px;margin-right: -20px;margin-left:-15px;">
+	<h1 title="گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار | entrepreneurship | professional | marketing ">مشاوره</h1>
+		<ul itemscope itemtype="http://schema.org/hasPart" itemprop="entrepreneurship" class="cols-4 ca-menu" style="margin-top:60px;margin-bottom:60px;margin-right: -20px;margin-left:-15px;">
 
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="financial" class="mmenus">
                     <li id="mali">
                         <a href="<?php echo $path; ?>financial" >
                             <div class="ca-content">
-                                 <h2 title="گروه حرفه ای مشاوران مالی کارآفرینی و کسب و کار ssd" class="ca-main">مالی</h2>
+<<<<<<< HEAD
+                                 <h2 title="گروه حرفه ای مشاوران مالی کارآفرینی و کسب و کار| financial | consulting ssd" class="ca-main">مالی</h2>
                                 <h3 title="نام مواد گروه حرفه ای مشاوران مالی کارآفرینی و کسب و کار ssd" class="ca-sub">
+=======
+                                <h2 title="استراتژی سازمانی ssd | marketing | strategy | organizational" class="ca-main" style="font-size:26px">استراتژی سازمانی</h2>
+                                <h3 title="شرح استراتژی و بازاریابی ssd" class="ca-sub">
+
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -115,12 +125,16 @@
                     </li>
 		</div>
 
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="management" class="mmenus">
                     <li id="mod">
                         <a href="<?php echo $path; ?>management" >
                             <div class="ca-content">
-                                <h2 title="گروه حرفه ای مشاوران مدیریتی کارآفرینی و کسب و کار ssd" class="ca-main">مدیریتی</h2>
+<<<<<<< HEAD
+                                <h2 title="گروه حرفه ای مشاوران مدیریتی کارآفرینی و کسب و کار ssd | management" class="ca-main">مدیریتی</h2>
                                 <h3 title="نام مواد گروه حرفه ای مشاوران مدیریتی کارآفرینی و کسب و کار ssd" class="ca-sub">
+=======
+                                <h2 title="بهبود عملکرد فرآیندهای سازمان کسب و کار ssd" class="ca-main">بهبود عملکرد</h2>
+                                <h3 title="ssd شرح بهبود عملکرد فرآیندهای سازمان کسب و کار" class="ca-sub">
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -134,12 +148,16 @@
                     </li>
 			</div>
 
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="technical" class="mmenus">
                     <li id="fani">
                         <a href="<?php echo $path; ?>technical" >
                             <div class="ca-content">
+<<<<<<< HEAD
                                 <h2 title="گروه حرفه ای مشاوران فنی کارآفرینی و کسب و کار ssd" class="ca-main">فنی</h2>
                                 <h3 title="نام مواد گروه حرفه ای مشاوران فنی کارآفرینی و کسب و کار ssd"  class="ca-sub">
+=======
+                                <h2 title="تکنولوژی نوین اطلاعات سازمانی | فناوری نوین" class="ca-main" style="font-size:26px">تکنولوژی نوین اطلاعات </h2>
+                                <h3 title="Technology | IT | information" class="ca-sub">
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -153,12 +171,21 @@
 			</div>
 
 					
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+<<<<<<< HEAD
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="consultation" class="mmenus">
                     <li id="education">
                         <a href="<?php echo $path; ?>ssd-consultation" >
                             <div class="ca-content">
                                 <h2 title="چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd" class="ca-main">چرا SSD</h2>
                                 <h3 title="شرح چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd" class="ca-sub">
+=======
+		<div class="mmenus">
+                    <li id="org">
+                        <a href="<?php echo $path; ?>ssd-consultation" >
+                            <div class="ca-content">
+                                <h2 title="سازمانی علم فروش و بازاریابی" class="ca-main">علم فروش و بازاریابی</h2>
+                                <h3 title="سازمانی شرح علم فروش و بازاریابی" class="ca-sub">
+
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -179,18 +206,26 @@
 	
 		<section id="Entrepreneurship">
 		
-				<h1 title="سامانه راهنمای مالکیت فکری، کارآفرینی و تجاری سازی گروه حرفه ای مشاوران SSD">کارآفرینی و کسب و کار</h1>
+				<h1 title="سامانه راهنمای مالکیت فکری، کارآفرینی و تجاری سازی گروه حرفه ای مشاوران SSD | commercialization">کارآفرینی و کسب و کار</h1>
 
 		<ul class="cols-4 ca-menu" style="margin-top:60px;margin-bottom:60px;margin-right: -20px;margin-left:-15px;">
 		
 
 			
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+<<<<<<< HEAD
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="business-knowledge" class="mmenus">
                     <li id="dan">
                         <a href="<?php echo $path; ?>business-knowledge" >
                             <div class="ca-content">
                                 <h2 title="دانستنی های کسب و کار و کارآفرینی ssd" class="ca-main">دانستنیهای کسب و کار</h2>
                                 <h3 title="شرح دانستنی های کسب و کار و کارآفرینی ssd" class="ca-sub">
+=======
+		<div class="mmenus">
+                    <li id="dan">
+                        <a href="<?php echo $path; ?>business-knowledge" >
+                            <div class="ca-content">
+                                <h2 title="رهبری کسب و کار و دارایی ها| leadership " class="ca-main" style="font-size:26px">رهبری کسب و کار و دارایی ها</h2>
+                                <h3 class="ca-sub">
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -204,12 +239,21 @@
                     </li>
 			</div>
 
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+<<<<<<< HEAD
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="business-clinic" class="mmenus">
                     <li id="clinic">
                         <a href="<?php echo $path; ?>business-clinic" >
                             <div class="ca-content">
                                 <h2 title="کلینیک کسب و کار و کارآفرینی ssd" class="ca-main" >کلینیک کسب و کار</h2>
                                 <h3 title="شرح کلینیک کسب و کار و کارآفرینی ssd" class="ca-sub">
+=======
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="social-media" class="mmenus">
+                    <li id="clinic">
+                        <a href="<?php echo $path; ?>business-clinic" >
+                            <div class="ca-content">
+                                <h2 title="فناوری شبکه های اجتماعی | تکنولوژی | social media" class="ca-main" style="font-size:26px">فناوری شبکه های اجتماعی</h2>
+                                <h3 class="ca-sub">
+
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -223,12 +267,21 @@
                     </li>
 			</div>
 
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+<<<<<<< HEAD
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="business-services" class="mmenus">
                     <li id="ser">
                         <a href="<?php echo $path; ?>business-services" >
                             <div class="ca-content">
                                 <h2 title="خدمات کارآفرینی و کسب و کار گروه حرفه ای مشاوران ssd" class="ca-main" style="font-size: 22px;">خدمات کارآفرینی و کسب و کار</h2>
                                 <h3 title="شرح خدمات کارآفرینی و کسب و کار گروه حرفه ای مشاوران ssd" class="ca-sub">
+=======
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="selling" class="mmenus">
+                    <li id="ser">
+                        <a href="<?php echo $path; ?>business-services" >
+                            <div class="ca-content">
+                                <h2 title="استراتژی رشد | علم فروش | بازاریابی" class="ca-main" style="font-size:24px">استراتژی رشد</h2>
+                                <h3 title="شرح استراتژی رشد | علم فروش | بازاریابی" class="ca-sub">
+>>>>>>> origin/master
 				<?php
 				mysql_query("SET CHARACTER SET utf8");   
 				mysql_query("SET NAMES utf8_persian_ci");
@@ -241,11 +294,18 @@
                     </li>								
 			</div>
 
-		<div  itemscope itemtype="http://schema.org/WebSite" itemprop="isPartOf" class="mmenus">
+<<<<<<< HEAD
+		<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="ssd-business" class="mmenus">
                     <li id="education">
                         <a href="<?php echo $path; ?>ssd-business" >
                             <div class="ca-content">
                                 <h2 title="چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd" class="ca-main">چرا SSD؟</h2>
+=======
+		<div itemscope itemtype="http://schema.org/isPartOf" itemprop="magazine" class="mmenus">
+                    <li id="entrepreneur">
+                        <a href="<?php echo $path; ?>ssd-business" >
+                            <div class="ca-content">
+                                <h2 title="مجله کارآفرینی و کسب و کار گروه حرفه ای مشاوران ssd | magazine" class="ca-main">مجله کارآفرینی</h2>
 
                                 <h3 title="شرح چرا گروه حرفه ای مشاوران کارآفرینی و راه اندازی کسب و کار ssd" class="ca-sub">
 				<?php
@@ -266,7 +326,7 @@
 		
 		</section>
 <section id="contact">
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="contactPoint" class="cols-2" style="margin-top:30px;">
+				<div  itemscope itemtype="http://schema.org/contactPoint" itemprop="contact" class="cols-2" style="margin-top:30px;">
 				
 				<div class="col" style="text-align:right;">
 					<h1 title="ارتباط با گروه حرفه ای مشاوران کارآفرینی و کسب و کار">ارتباط با ما</h1>
@@ -286,28 +346,28 @@
 				$ins=$row['ins'];
 				?>
 				
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="address" style="direction:rtl;"><i class="fa fa-map-marker"></i> <?php echo $address; ?></div><br>
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="telephone" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/PostalAddress" itemprop="address" style="direction:rtl;"><i class="fa fa-map-marker"></i> <?php echo $address; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/telephone" itemprop="phone" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone; ?></div><br>
 				<?php if($phone4==""){}else{?> 
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="telephone" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone4; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/telephone" itemprop="phone4" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone4; ?></div><br>
                                 <?php } ?>
 				<?php if($phone5==""){}else{?> 
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="telephone" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone5; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/telephone" itemprop="phone5" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone5; ?></div><br>
                                 <?php } ?>
 				<?php if($phone6==""){}else{?> 
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="isPartOf" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone6; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="phone6" style="direction:rtl;"><i class="fa fa-phone"></i>  <?php echo $phone6; ?></div><br>
                                 <?php } ?>
 
 				<?php if($phone2==""){}else{?> 
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="isPartOf" style="direction:rtl;"><i class="fa fa-fax"></i>  <?php echo $phone2; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="phone2" style="direction:rtl;"><i class="fa fa-fax"></i>  <?php echo $phone2; ?></div><br>
                                 <?php } ?>
 
 				<?php if($phone3==""){}else{?> 
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="isPartOf" style="direction:rtl;"><i class="fa fa-paper-plane"></i>  <?php echo $phone3; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="phone3" style="direction:rtl;"><i class="fa fa-paper-plane"></i>  <?php echo $phone3; ?></div><br>
                                 <?php } ?>
 
                                 <div style="direction:rtl;"><i class="fa fa-envelope"></i>  <?php echo $email; ?></div><br>
-				<div  itemscope itemtype="http://schema.org/Organization" itemprop="isPartOf" style="direction:rtl;"><i class="fa fa-instagram"></i> <?php echo $ins; ?></div><br>
+				<div  itemscope itemtype="http://schema.org/isPartOf" itemprop="instagram" style="direction:rtl;"><i class="fa fa-instagram"></i> <?php echo $ins; ?></div><br>
 
 				</div>
 				
