78328b6b2b6e9b49c4928cc891d1932f74332caf
Kamali <kamali.fateme@gmail.com>
2017-05-30 14:04:50 +0430

    اضافه کردن سرچ و کار کردن بر روی css
    کار css نا تمام

--- domains/ssd-cg.com/public_html/ssd/tophead.php
+++ domains/ssd-cg.com/public_html/ssd/tophead.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='utf-8'>
+</head>
+<body>
 <div class="pos" >
 <img src="<?php echo $path; ?>img/loo.png" style="height:22px;width:auto;margin-right: 20px;    margin-top: 8px;" >
 <a itemscope itemtype="http://schema.org/url" itemprop="mainpage" href="http://www.ssd-cg.com/ssd/">صفحه اصلی</a> |	<a itemscope itemtype="http://schema.org/url" itemprop="about-us" href="http://www.ssd-cg.com/ssd/about-us">درباره ما</a> | <a itemscope itemtype="http://schema.org/url" itemprop="contact-us" href="contact-us">ارتباط با ما</a>
@@ -6,3 +15,21 @@
 <div class="top-head" style="margin-left:-30px;margin-right:-30px;">
 			<a itemscope itemtype="http://schema.org/url" itemprop="contactus" href="<?php echo $path; ?>">صفحه اصلی</a> |	<a itemscope itemtype="http://schema.org/url" itemprop="aboutus" href="<?php echo $path; ?>about-us">درباره ما</a> | <a href="contact-us">ارتباط با ما</a>
 			</div>
+            		<div class="box1" style="margin-top:7px;">
+<script>
+  (function() {
+    var cx = '015530550003775781693:no49ad5dzvq';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+</div>
+</body>
+</html>
+
+
